4bef6007ccd3baf73cb2768333001103769d7df0
Miroslav Milivojevic <miroslav.milivojevic@adapptiv.uk>
2024-10-06 07:34:23 +0200

    add simple hello world code

---
+++ src/hello.cpp
@@ -0,0 +1,7 @@
+// src/hello.cpp
+#include "hello.h"
+#include <iostream>
+
+void say_hello() {
+    std::cout << "Hello, World from Meson Library!" << std::endl;
+}
---
+++ src/hello.h
@@ -0,0 +1,7 @@
+// src/hello.h
+#ifndef HELLO_H
+#define HELLO_H
+
+void say_hello();
+
+#endif // HELLO_H
